0796a4f5aec608acb17cbe0e5c378800e3711da0
ConorSM <89384242+ConorSM@users.noreply.github.com>
2021-09-16 15:35:06 +0100

    Add files via upload

--- password_checker.py
+++ password_checker.py
@@ -1,4 +1,5 @@
 from password_package import password_module3
+from password_package import password_generator
 import getpass
 import sqlite3
 from contextlib import closing
@@ -9,9 +10,17 @@ Password Policy:
 - Password must be at least 8 characters long
 - Password must contain at least one upper case letter, and one lower case letter
 - Password must contain at least one integer
-- Password must contain at least one special character from [!_@$#*+-?~]
+- Password must contain at least one special character from [!_@$#*+-?~<^>|]
 
 """
+
+print("Welcome to the password stength checker / generator!")
+
+def user_choice():
+    global choice
+
+    choice = input("Press 1 to create own password. Press 2 to use password generator. Press 3 to exit: ")
+    choice = int(choice)
 
 def user_input():
     global fname
@@ -41,72 +50,67 @@ def user_input():
         else:
             exit()
 
-def user_choice():
-    global choice
 
-    choice = input("Press 1 to create own password. Press 2 to use password generator. Press 3 to exit: ")
-    choice = int(choice)
+try:
 
-
-# try:
-#     user_input()
-#     user_choice()
-#
-#      if choice == 1:
-#         password = password_module3.get_pass()
-#         password = password_module3.password_constraints(password)
-#         password = password_module3.password_username(password, fname, lname, birthdate)
-#         password_valid = password_module3.common_passwords(password)
-#         print(password_module3.password_strength(password_valid))
-#
-#     elif choice == 2:
-#         pass
-#
-#     elif choice == 3:
-#         pass
-#
-#     else:
-#         user_choice()
-
-#     export_file = input("Would you like to export results to a text file? y/n: ")
-#     export_file = export_file.lower()
-#
-#     if export_file == "y":
-#         file = open("result.txt", "w")
-#         data = file.write(results)
-#         file.close()
-#
-#     elif export_file == "n":
-#         print("Goodbye!")
-
-
-# except Exception as e:
-#     print("Error! Try again")
-
-user_input()
-
-user_choice()
-
-if choice == 1:
-    password = password_module3.get_pass()
-    password = password_module3.password_constraints(password)
-    password = password_module3.password_username(password, fname, lname, birthdate)
-    password_valid = password_module3.common_passwords(password)
-    print(password_module3.password_strength(password_valid))
-elif choice == 2:
-     pass
-elif choice == 3:
-     pass
-else:
     user_choice()
 
+    if choice == 1:
+        user_input()
+        password = password_module3.get_pass()
+        password = password_module3.password_constraints(password)
+        password = password_module3.password_username(password, fname, lname, birthdate)
+        password_valid = password_module3.common_passwords(password)
+        print(password_module3.password_strength(password_valid))
+        result = password_module3.password_strength(password_valid)
+    elif choice == 2:
+         password_generator.generate_password()
+    elif choice == 3:
+         pass
+    else:
+        user_choice()
+
+    export_file = input("Would you like to export results to a text file? y/n: ")
+    export_file = export_file.lower()
+
+    if export_file == "y":
+        file = open("result.txt", "w")
+        data = file.write(result)
+        file.close()
+        print("Results successfuly exported to a text file")
+
+    elif export_file == "n":
+        print("Goodbye!")
+
+except Exception as e:
+    print("Error! Try again")
+
+
+# user_choice()
+#
+# if choice == 1:
+#     user_input()
+#     password = password_module3.get_pass()
+#     password = password_module3.password_constraints(password)
+#     password = password_module3.password_username(password, fname, lname, birthdate)
+#     password_valid = password_module3.common_passwords(password)
+#     print(password_module3.password_strength(password_valid))
+#     result = password_module3.password_strength(password_valid)
+# elif choice == 2:
+#      password_generator.generate_password()
+# elif choice == 3:
+#      pass
+# else:
+#     user_choice()
+#
 # export_file = input("Would you like to export results to a text file? y/n: ")
-#     export_file = export_file.lower()
+# export_file = export_file.lower()
 #
-#     if export_file == "y":
-#         file = open("result.txt", "w")
-#         data = file.write(results)
-#         file.close()
+# if export_file == "y":
+#     file = open("result.txt", "w")
+#     data = file.write(result)
+#     file.close()
+#     print("Results successfuly exported to a text file")
 #
-#     elif export_file == "n":
-#         print("Goodbye!")
+# elif export_file == "n":
+#     print("Goodbye!")
--- password_package/password_module3.py
+++ password_package/password_module3.py
@@ -3,35 +3,35 @@ import getpass
 def get_pass():
      print("Password must be between 8 and 18 characters long")
      print("Password must contain at least one upper case letter, and one lower case letter")
-     print("Password must contain at least one integer, and one special character from [!_@$#*+-?~]")
+     print("Password must contain at least one integer, and one special character from [!_@$#*+-?~|^]")
      password = getpass.getpass("Please enter password: ")
      return password
 
 # Has to meet constraints to be valid password
 
-#make it so you can re-enter password if constraint not met
+# make it so you can re-enter password if constraint not met
 
 def password_constraints(password):
-    special_sym = ["!", "_", "$", "@", "*", "&", "#", "~", "?"]
+    special_sym = ["!", "_", "$", "@", "*", "&", "#", "~", "?", "|", ":", "^", "+"]
 
     if len(password) < 8:
         print("Password length should be at least 8")
-
+        exit()
     elif len(password) > 18:
         print("Password length should be not be greater than 18")
-
+        exit()
     elif not any(char.isdigit() for char in password):
         print("Password should have at least one numeral")
-
+        exit()
     elif not any(char.isupper() for char in password):
         print("Password should have at least one uppercase letter")
-
+        exit()
     elif not any(char.islower() for char in password):
         print("Password should have at least one lowercase letter")
-
+        exit()
     elif not any(char in special_sym for char in password):
         print("Password should have at least one of the symbols [!_@$#*+-?~]")
-
+        exit()
     else:
         return password
 
@@ -43,9 +43,11 @@ def password_username(password, fname, lname, birthdate):
 
     if fname_lower in password_lower or lname_lower in password_lower:
         print("Password can not contain first or last name!")
+        exit()
 
     elif str(birthdate) in password:
         print("Password can not contain birth date!")
+        exit()
     else:
         return password
 
@@ -104,12 +106,6 @@ def common_passwords(password):
     'silver',
 )
 
-#    print(most_common_passwords[0])
-
-# def password_checker():
-#     password = input("\nEnter in your password: ")
-#     print(f"\nYour password is {password}.\n")
-
     common_password_character_checker = any(item in password for item in most_common_passwords)
 
     if password == password in most_common_passwords:
@@ -122,25 +118,14 @@ def common_passwords(password):
         print("Your password has been accepted")
         return password
 
-# error if password contains item from common list
-
-
-
-
-#can remove some if the minimum valid password is 8 char long
-
-
-
-
-
 def password_strength(password):
     if len(password) > 12:
-        result = "Your password strength is strong. Password strength is usually measured in terms of information entropy. The length of a password is by far the largest determinant of entropy. The longer a password is, the higher the entropy, and thus the less likely it is to succumb to brute-force searches. Passwords chosen by humans tend to be more easily brute-forced than entropy suggests, since they often contain commonly used phrases, as well as information relating to the user's name or birth date. This programme automatically rejects such passwords for this very reason.  "
+        result = "Your password strength is strong. \n Password strength is usually measured in terms of information entropy. The length of a password is by far the largest determinant of entropy. The longer a password is, the higher the entropy, and thus the less likely it is to succumb to brute-force searches. Passwords chosen by humans tend to be more easily brute-forced than entropy suggests, since they often contain commonly used phrases, as well as information relating to the user's name or birth date. This programme automatically rejects such passwords for this very reason.  "
         return result
 
     elif len(password) > 10 and len(password) <= 12:
-        result = "Your password strength is medium. Password strength is usually measured in terms of information entropy. The length of a password is by far the largest determinant of entropy. The longer a password is, the higher the entropy, and thus the less likely it is to succumb to brute-force searches. Passwords chosen by humans tend to be more easily brute-forced than entropy suggests, since they often contain predictable phrases and information relating to the user's name or birth date. This programme automatically rejects such passwords for this very reason. "
+        result = "Your password strength is medium. \n Password strength is usually measured in terms of information entropy. The length of a password is by far the largest determinant of entropy. The longer a password is, the higher the entropy, and thus the less likely it is to succumb to brute-force searches. Passwords chosen by humans tend to be more easily brute-forced than entropy suggests, since they often contain predictable phrases and information relating to the user's name or birth date. This programme automatically rejects such passwords for this very reason. "
         return result
     else:
-        result = "Your password strength is weak. Password strength is usually measured in terms of information entropy. The length of a password is by far the largest determinant of entropy. The longer a password is, the higher the entropy, and thus the less likely it is to succumb to brute-force searches. Passwords chosen by humans tend to be more easily brute-forced than entropy suggests, since they often contain predictable phrases and information relating to the user's name or birth date. This programme automatically rejects such passwords for this very reason. "
+        result = "Your password strength is weak. \n Password strength is usually measured in terms of information entropy. The length of a password is by far the largest determinant of entropy. The longer a password is, the higher the entropy, and thus the less likely it is to succumb to brute-force searches. Passwords chosen by humans tend to be more easily brute-forced than entropy suggests, since they often contain predictable phrases and information relating to the user's name or birth date. This programme automatically rejects such passwords for this very reason. "
         return result
